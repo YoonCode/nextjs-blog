46bc5134045694cea5ccb3e48710ac619c8b714d
coder.yoon <yjin830201@gmail.com>
2023-03-28 23:36:23 +0900

    about, contact page done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,30 @@
+import Hero from "../components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
+
 export default function AboutPage() {
-  return <h1>about Sangjin</h1>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          소통과 문제해결의 전문 개발자 <br />
+          성장을 위한 끊임없는 도전과 학습 과정을 즐기는 개발자
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          삼백씨비티 (-Now) <br />
+          투티 (-2023.03) <br />
+          미션1 (-2021.12)
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>
+          React, Typescript, Node <br />
+          Git, Postman, Firebase <br />
+          VS Code, MongoDB
+        </p>
+      </section>
+    </>
+  );
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,34 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import ContactForm from "../components/ContactForm";
+
+const LINKS = [
+  { icon: <AiFillGithub />, url: "https://github.com/YoonCode" },
+  {
+    icon: <AiFillLinkedin />,
+    url: "https://www.linkedin.com/in/sangjin-yoon/",
+  },
+];
+
 export default function ContactPage() {
-  return <h1>contact Sangjin</h1>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2>Contact Me</h2>
+      <p>yooncode.dev@gmail.com</p>
+      <ul>
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-yellow-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
